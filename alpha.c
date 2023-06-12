d005b9293afe53cf61539cade49f7e6ce4992c39
GAGAN <100433044+Gagankali@users.noreply.github.com>
2023-06-12 15:07:12 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -73,3 +73,23 @@ int main() {
     
     return 0;
 }. */
+
+
+
+
+
+
+/*#include<stdio.h>
+int main(){
+  int n,rem,rev=0;
+  printf("Enter the no :");
+  scanf("%d",&n);
+  while(n>0)
+    {
+      rem=n%10;
+      rev =rev*10+rem;
+      n=n/10;
+    }
+  printf("Reverse no is %d ",rev);
+  return 0;
+}. */
