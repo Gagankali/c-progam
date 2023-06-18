96fbd835ea564598a07004a8dc488dee2507288a
GAGAN <100433044+Gagankali@users.noreply.github.com>
2023-06-18 14:25:05 +0530

    Develop a C Program to create identity matrix

--- identitymatrix1111.c
+++ identitymatrix1111.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+int main()
+{
+int i, j, n;
+ printf("Enter the dimension of the matrix: ");
+ scanf("%d", &n);
+ for(i = 0; i < n;i++)
+ {
+ for(j = 0; j < n;j++)
+{
+ if(i == j)
+{
+ printf("1 ");
+ }
+else
+{
+ printf("0 ");
+ }
+ }
+ printf("\n");
+ }
+ return 0;
+}
