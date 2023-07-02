72ffdb00ff7d5c833f2b15c9629a53e945f0b9e8
GAGAN <100433044+Gagankali@users.noreply.github.com>
2023-07-02 21:47:03 +0530

    Update multiplytwomatrices33333.c

--- multiplytwomatrices33333.c
+++ multiplytwomatrices33333.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+void main()
 {
     int a[10][10], b[10][10], mul[10][10], m, n, p, q, i, j, k;
     printf("enter the order of Matrix A:-");
@@ -37,64 +36,6 @@ int main()
         }
     }
     for (i = 0; i < m; i++)
-        printf("enter the first matrix element:-\n");
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    }
-    printf("enter the second matrix element\n");
-    for (i = 0; i < p; i++)
-    {
-        for (j = 0; j < q; j++)
-        {
-            scanf("%d", &b[i][j]);
-        }
-    }
-    printf("multiply of the matrix\n");
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < q; j++)
-        {
-            mul[i][j] = 0;
-            for (k = 0; k < p; k++)
-            {
-                mul[i][j] = mul[i][j] + a[i][k] * b[k][j];
-            }
-        }
-    }
-    for (i = 0; i < m; i++)
-        printf("enter the first matrix element:-\n");
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    }
-    printf("enter the second matrix element\n");
-    for (i = 0; i < p; i++)
-    {
-        for (j = 0; j < q; j++)
-        {
-            scanf("%d", &b[i][j]);
-        }
-    }
-    printf("multiply of the matrix\n");
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < q; j++)
-        {
-            mul[i][j] = 0;
-            for (k = 0; k < p; k++)
-            {
-                mul[i][j] = mul[i][j] + a[i][k] * b[k][j];
-            }
-        }
-    }
-    for (i = 0; i < m; i++)
     {
         for (j = 0; j < n; j++)
         {
@@ -102,5 +43,4 @@ int main()
         }
         printf("\n");
     }
-    return 0;
 }
