58da1442a63862a99c1e9c2f166a0cabe16353cd
GAGAN <100433044+Gagankali@users.noreply.github.com>
2023-07-03 15:09:23 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -81,3 +81,39 @@ else
 printf("Record Not Found\n");
   }
 }
+
+
+
+
+linear search 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+ void main()
+{
+ int a[20],i,x,n;
+ printf("How many contact numbers?\n");
+ scanf("%d",&n);
+ printf("Enter contact numbers \n");
+ for(i=0;i<n;++i)
+ scanf("%d",&a[i]);
+ printf("nEnter number to search:");
+ scanf("%d",&x);for(i=0;i<n;++i)
+
+ {
+
+ if(a[i]==x) 
+
+{
+
+ printf("Element found at index %d",i);
+
+ exit(0);
+
+ }
+
+ }
+
+ printf("Element not found");
+}
