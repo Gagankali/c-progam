99fb57308837a207fd57e691028788bf8e962730
GAGAN <100433044+Gagankali@users.noreply.github.com>
2023-07-03 14:59:22 +0530

    Update week.c

--- week.c
+++ week.c
@@ -1,4 +1,6 @@
 /*
+ 1: Develop a program using bubble sort technique to display the prices of each item from 
+lowest to highest
 bubble sort
 
 #include<stdio.h>
@@ -66,3 +68,59 @@ printf("\n Prices of each items from Lowest to Highest are :-\n");
  printf("%d\t",items[i]);
  }
 }.        */
+
+
+
+
+/*
+
+2: Develop a program using selection sort technique to display the weights of each item 
+from lowest to highest
+
+#include <stdio.h>
+void main()
+{
+int a[100], n, i, j, position, swap;
+printf("Enter number of items\n");
+scanf("%d", &n);
+printf("Enter weight of each items\n");
+for (i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+for(i = 0; i < n - 1; i++)
+{
+position=i;
+for(j = i + 1; j < n; j++){
+
+if(a[position] > a[j])
+
+position=j;
+
+}
+
+if(position != i)
+
+{
+
+swap=a[i];
+
+a[i]=a[position];
+
+a[position]=swap;
+
+}
+
+}
+
+printf("After sorting weights of each item:\n");
+
+for(i = 0; i < n; i++)
+
+{
+
+printf("%d\t", a[i]);
+
+}
+
+}.  */
